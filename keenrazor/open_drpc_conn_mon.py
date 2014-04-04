633b876e367cbdc41570d699019fe3f4768d6b2b
Daniel Kador <dkador@gmail.com>
2014-04-04 14:58:58 -0700

    strip dots out of prop names for hosts

--- keenrazor/open_drpc_conn_mon.py
+++ keenrazor/open_drpc_conn_mon.py
@@ -46,12 +46,18 @@ def handle_results(results, options):
         emailer.Emailer.send_email(addr_to=options.email, subject="DRPC Server Open Conns Too High",
                                    text=text, categories=["open_drpc_conn_mon"])
 
+    replaced_hosts = {replace_dots_from_prop_name(host): num_conns for (host, num_conns) in results.iteritems()}
+
     keen.add_event("open_drpc_conn_mon", {
         "maxconns": options.maxconns,
         "over_limit_hosts": over_limit_hosts,
         "all_under_limit": all_under_limit,
-        "results": results
+        "results": replaced_hosts
     })
+
+
+def replace_dots_from_prop_name(prop_name):
+    return prop_name.replace(".", "_")
 
 
 def main(args=sys.argv):
